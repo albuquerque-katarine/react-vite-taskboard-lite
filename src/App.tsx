86d3787cc74f5360348e2ac32ef91b0967f1f255
albuquerque-katarine <kba.2879@gmail.com>
2026-07-15 20:19:54 -0300

    Ajuste nas rotas 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { useTheme } from "./contexts/ThemeContext";
-import Router from "./routes"
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import NewTask from "./pages/NewTask";
+import DetailTask from "./pages/DetailTask";
+import About from "./pages/About";
+import Custom404 from "./pages/404";
+import Custom500 from "./pages/500";
 
 function App() {
 
@@ -8,7 +16,19 @@ function App() {
 
   return (
     <div className={`${theme === "light" ? "bg-gray-color h-[100vh]" : "bg-white h-[100vh]"} h-[100%]`}>      
-      <Navbar/>        
+      <Navbar/> 
+      <BrowserRouter>        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/new" element={<NewTask />} />
+          <Route path="/detail/:id" element={<DetailTask />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Custom404 />} />
+          <Route path="/500" element={<Custom500 />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>       
     </div>
   )
 }
